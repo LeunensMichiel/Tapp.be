e7594edd62a4dee768f7f0dbbedb1a2c351ce5c7
Michiel Leunens <michiel.leunens@gmail.com>
2020-06-28 17:56:08 +0200

    Added Jetpack/Material bottom navigation

--- app/src/main/java/com/leunesmedia/tappbe/MainActivity.kt
+++ app/src/main/java/com/leunesmedia/tappbe/MainActivity.kt
@@ -2,12 +2,20 @@ package com.leunesmedia.tappbe
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.main_activity.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
+        setupViews()
+    }
 
+    private fun setupViews() {
+        val navController = findNavController(R.id.nav_host_fragment)
+        bottomNavigationView.setupWithNavController(navController)
     }
 }
